56f8067f16b8806ac789f72eec26e0face00b35f
zn508116 <zn508116@etu.unice.fr>
2018-10-25 17:46:27 +0200

    Test updateScore(botScore, roll) de ScoreDice

---
+++ src/test/java/engine/ScoreCounterTest.java
@@ -0,0 +1,43 @@
+package engine;
+
+import bot.BotScore;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScoreCounterTest {
+
+    @Test
+    public void ScoreDiceTest() {
+
+        BotScore bs = new BotScore();
+        ScoreCounter sc = new ScoreCounter();
+
+        sc.updateScore(bs, "1@SOLAR%2@VICTORY");
+        Assert.assertEquals(1, bs.getSolar());
+        Assert.assertEquals(2, bs.getVictory());
+
+        sc.updateScore(bs, "1@SOLAR%2@VICTORY");
+        Assert.assertEquals(2, bs.getSolar());
+        Assert.assertEquals(4, bs.getVictory());
+
+        sc.updateScore(bs, "1@GOLD%1@GOLD");
+        Assert.assertEquals(2, bs.getGold());
+
+        sc.updateScore(bs, "1@SOLAR%1@GOLD");
+        Assert.assertEquals(3, bs.getSolar());
+        Assert.assertEquals(3, bs.getGold());
+
+        sc.updateScore(bs, "1@SOLAR%1@LUNAR");
+        Assert.assertEquals(4, bs.getSolar());
+        Assert.assertEquals(1, bs.getLunar());
+
+        sc.updateScore(bs, "1@GOLD%1@LUNAR");
+        Assert.assertEquals(4, bs.getGold());
+        Assert.assertEquals(2, bs.getLunar());
+
+        sc.updateScore(bs, "2@VICTORY%1@SOLAR");
+        Assert.assertNotEquals(6, bs.getVictory()); // d1 de la string roll doit être un dé solar
+        Assert.assertNotEquals(5, bs.getSolar()); // d2 de la string roll doit être un dé lunar
+
+    }
+}
