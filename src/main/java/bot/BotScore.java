ef1bded8951867d98a501d60555d80e84afa75d2
Alex <alex.denos@laposte.net>
2018-10-25 17:16:29 +0200

    Test BotScore

--- src/main/java/bot/BotScore.java
+++ src/main/java/bot/BotScore.java
@@ -51,6 +51,7 @@ public class BotScore {
     }
 
     public void addVictory(int victory) {
+        victory = Math.abs(victory);
         this.victory += victory;
     }
 
---
+++ src/test/java/bot/BotScoreTest.java
@@ -0,0 +1,50 @@
+package bot;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BotScoreTest {
+
+    @Test
+    public void BotScoreTest() {
+
+        // Test BotScore addGold
+        BotScore bs = new BotScore();
+        bs.addGold(12);
+        Assert.assertEquals(12,bs.getGold());
+        bs.addGold(2000);
+        Assert.assertEquals(12,bs.getGold());
+        bs.addGold(-5000);
+        Assert.assertEquals(12,bs.getGold());
+
+        // Test BotScore addSolar
+        bs.addSolar(6);
+        Assert.assertEquals(6,bs.getSolar());
+        bs.addSolar(2000);
+        Assert.assertEquals(6,bs.getSolar());
+        bs.addSolar(-5000);
+        Assert.assertEquals(6,bs.getSolar());
+
+        // Test BotScore addLunar
+        bs.addLunar(6);
+        Assert.assertEquals(6,bs.getLunar());
+        bs.addLunar(2000);
+        Assert.assertEquals(6,bs.getLunar());
+        bs.addLunar(-5000);
+        Assert.assertEquals(6,bs.getLunar());
+
+        // Test BotScore addVictory
+        bs.addVictory(-5000);
+        Assert.assertEquals(5000,bs.getVictory());
+
+
+    }
+
+
+
+
+
+
+
+
+}
