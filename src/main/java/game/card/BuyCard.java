21cabcff569bbe5bc1345532745aca4ed48510b5
zn508116 <zn508116 @etu.unice.fr>
2018-11-14 10:07:47 +0100

    ajout paiement carte et gain de point de victoire #10

--- src/main/java/game/card/BuyCard.java
+++ src/main/java/game/card/BuyCard.java
@@ -1,13 +1,17 @@
 package game.card;
 
 import game.BotScore;
+import game.ScoreCounter;
 
 public class BuyCard {
 
-    public static boolean buyCard(Islands islands, NeutralCard card, BotScore botScore){
+    public static boolean buyCard(Islands islands, NeutralCard card, BotScore botScore, ScoreCounter scoreCounter){
         if(card.getPrice()[0] <= botScore.getSolar() && card.getPrice()[1] <= botScore.getLunar()) {
             for (AbstractCard i : islands.getIslands().get(Math.max(card.getPrice()[0],card.getPrice()[1]))) {
                 if(i != null && i.equals(card)) {
+                    scoreCounter.paySolar(botScore, card.getPrice()[0]);
+                    scoreCounter.payLunar(botScore, card.getPrice()[1]);
+                    scoreCounter.addVictory(botScore, card.getVictory());
                     return true;
                 }
             }
