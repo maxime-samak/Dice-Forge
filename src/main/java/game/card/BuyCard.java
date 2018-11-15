665d25a9da31c160dc4fe0ada99f8c5f74302edd
zn508116 <zn508116@etu.unice.fr>
2018-11-15 16:37:56 +0100

    Correction paramètre scoreCounter Création BuyCardTest #10 #15

--- src/main/java/game/card/BuyCard.java
+++ src/main/java/game/card/BuyCard.java
@@ -13,13 +13,13 @@ public class BuyCard {
      * @param scoreCounter
      * @return
      */
-    public static boolean buyCard(Islands islands, NeutralCard card, BotScore botScore, ScoreCounter scoreCounter){
+    public static boolean buyCard(Islands islands, NeutralCard card, BotScore botScore){
         if(card.getPrice()[0] <= botScore.getSolar() && card.getPrice()[1] <= botScore.getLunar()) {
             for (AbstractCard i : islands.getIslands().get(Math.max(card.getPrice()[0],card.getPrice()[1]))) {
                 if(i != null && i.equals(card)) {
-                    scoreCounter.paySolar(botScore, card.getPrice()[0]);
-                    scoreCounter.payLunar(botScore, card.getPrice()[1]);
-                    scoreCounter.addVictory(botScore, card.getVictory());
+                    ScoreCounter.paySolar(botScore, card.getPrice()[0]);
+                    ScoreCounter.payLunar(botScore, card.getPrice()[1]);
+                    ScoreCounter.addVictory(botScore, card.getVictory());
                     return true;
                 }
             }
