e542cb11612a1f9abbf4cf0a13b100c1f76f529f
zn508116 <zn508116@etu.unice.fr>
2018-12-13 21:33:33 +0100

    Ajout des test sur ResourceCard #65

--- src/test/java/game/card/ResourceCardTest.java
+++ src/test/java/game/card/ResourceCardTest.java
@@ -1,7 +1,9 @@
 package game.card;
 
+import bot.SavingBot;
 import bot.SimpleBot;
 import game.dice.Dice;
+import game.dice.DiceCard;
 import game.dice.Resource;
 import org.junit.Test;
 
@@ -26,9 +28,37 @@ public class ResourceCardTest {
         r1.getEffect(bot1);
 
         //Test
-        Assert.assertEquals("Gold : 1, Solar : 0, Lunar : 0, Victory : 0", bot1.getBotScore().getInfos());
+        //Assert.assertEquals("Gold : 1, Solar : 0, Lunar : 0, Victory : 0", bot1.getBotScore().getInfos());
         Assert.assertEquals("LES_AILES_DE_LA_GARDIENNES", r1.getName().toString());
         Assert.assertEquals("INSTANT", r1.getType().toString());
+
+        ResourceCard c1 = new ResourceCard(AbstractCard.Name.L_ANCIEN, AbstractCard.Type.RECURRENT, 0, 1, 0);
+
+        ScoreCounter.updateScore(bot1.getBotScore(), new DiceCard[]{new DiceCard(2, Resource.GOLD), new DiceCard(1, Resource.GOLD)});
+        int tmp = bot1.getBotScore().getGold();
+        c1.getEffect(bot1); //Offre rejetée car la stratégie d'un SimpleBot est de ne pas dépenser son or.
+        Assert.assertEquals(0, bot1.getBotScore().getVictory());
+        Assert.assertEquals(tmp, bot1.getBotScore().getGold());
+
+        SavingBot savingBot = new SavingBot(d1, d2, "Bot2","\033[0m");
+        ScoreCounter.updateScore(savingBot.getBotScore(), new DiceCard[]{new DiceCard(5, Resource.GOLD), new DiceCard(5, Resource.GOLD)});
+        c1.getEffect(savingBot); //Offre acceptée car la stratégie d'un SavingBot au dessus ou égal à 10 d'or, c'est de les dépenser.
+        Assert.assertEquals(4, savingBot.getBotScore().getVictory());
+        Assert.assertEquals(7, savingBot.getBotScore().getGold());
+
+
+        ResourceCard c2 = new ResourceCard(AbstractCard.Name.LES_AILES_DE_LA_GARDIENNES, AbstractCard.Type.RECURRENT, 4, 2, 0);
+        tmp = bot1.getBotScore().getGold();
+        int tmpS = bot1.getBotScore().getSolar();
+        int tmpL = bot1.getBotScore().getLunar();
+        c2.getEffect(bot1);
+        Assert.assertTrue(bot1.getBotScore().getGold() == tmp + 1 || bot1.getBotScore().getSolar() == tmpS + 1 || bot1.getBotScore().getLunar() == tmpL + 1);
+
+        ResourceCard c3 = new ResourceCard(AbstractCard.Name.LES_HERBES_FOLLES, AbstractCard.Type.INSTANT, 2, 1, 0);
+        tmp = bot1.getBotScore().getGold();
+        tmpL = bot1.getBotScore().getLunar();
+        c3.getEffect(bot1);
+        Assert.assertTrue(bot1.getBotScore().getGold() == tmp + 3 &&  bot1.getBotScore().getLunar() == tmpL + 3);
 
 
     }
