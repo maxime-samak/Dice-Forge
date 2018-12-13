66837dedcf0ee938f5e7ff683ab50185354649f7
erdaltoprak <10966155+erdaltoprak@users.noreply.github.com>
2018-12-13 23:24:02 +0100

    #65 Ajouts complémentaires dans CardTest

--- src/test/java/game/card/CardTest.java
+++ src/test/java/game/card/CardTest.java
@@ -21,14 +21,23 @@ public class CardTest {
         Card c3 = new RegularCard(AbstractCard.Name.L_HYDRE, AbstractCard.Type.INSTANT, 26, 5, 5);
 
         Assert.assertEquals(12, c1.getVictory());
-        Assert.assertEquals(14, c2.getVictory());
-        Assert.assertEquals(26, c3.getVictory());
-
         Assert.assertEquals(0, c1.getPrice()[0]);
         Assert.assertEquals(4, c1.getPrice()[1]);
+        Assert.assertEquals("LE_PASSEUR", c1.getName().toString());
+        Assert.assertEquals("INSTANT", c1.getType().toString());
 
+        Assert.assertEquals(14, c2.getVictory());
+        Assert.assertEquals(4, c2.getPrice()[0]);
+        Assert.assertEquals(0, c2.getPrice()[1]);
+        Assert.assertEquals("LA_MEDUSE", c2.getName().toString());
+        Assert.assertEquals("INSTANT", c2.getType().toString());
+
+        Assert.assertEquals(26, c3.getVictory());
         Assert.assertEquals(5, c3.getPrice()[0]);
         Assert.assertEquals(5, c3.getPrice()[1]);
+        Assert.assertEquals("L_HYDRE", c3.getName().toString());
+        Assert.assertEquals("INSTANT", c3.getType().toString());
+
 
         Dice d1 = new Dice();
         d1.solarDiceInit();
